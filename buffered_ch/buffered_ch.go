9562f392621fe74edd5debf9947c11ce917ad0fc
revant <revantjha.94@gmaiml.com>
2017-05-23 18:46:37 +0530

    channels

---
+++ buffered_ch/buffered_ch.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg = sync.WaitGroup{}
+
+func main() {
+	ch := make(chan int) //channel is unbuffered by default
+	// unbuffered: channel will only send if there is something to recieve , else deadlock error
+	wg.Add(2)
+	go pushinto(ch)
+	go pullfrom(ch)
+	wg.Wait()
+
+}
+
+// send only
+// best practise to use only one type of channel in one go routine , avoid using both sending and recieving form
+//the same channel in the same go routine
+func pushinto(ch chan<- int) { // can use (ch chan int ) also but this way we can see if which end of the chanel we are using
+	i := 42
+	ch <- i
+	wg.Done()
+}
+
+//recieve only
+func pullfrom(ch <-chan int) {
+	a := <-ch
+	fmt.Println(a)
+	wg.Done()
+}
